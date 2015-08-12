bc458255f15045a3cbb30a2f4a31b7a7c71fc7e2
Yaolong Huang <airekans@gmail.com>
2015-08-12 12:00:49 +0800

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,9 +1,28 @@
+class Process(object):
+    def __init__(self):
+        pass
+    
+    def process(self, input):
+        pass
+
+def sent_output(process_id, output, processes):
+    for target_process, msg in output.iteritems():
+        distance = process_link[(process_id, target_process)]
+        process_input_queue[target_process][distance].append(msg)
+
+def goto_next_round(next_round):
+    pass
 
 def main():
     processes = []
     
-    # stage 1: process input and 
-    for process in processes
+    while True:
+        for i, process in enumerate(processes):
+            input_queue = process.get_input_queue()
+            output = process.process(input_queue)
+            sent_output(i, output, processes)
+            next_round = raw_input('next_round[default:1]: ')
+            goto_next_round(next_round)
     
     return
 
