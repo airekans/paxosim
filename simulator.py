40d100e52aa47c730c89166d0d6f70d59598142a
Yaolong Huang <airekans@gmail.com>
2015-08-12 15:02:00 +0800

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,3 +1,6 @@
+import sys
+import importlib
+
 class Process(object):
     def __init__(self):
         pass
@@ -17,33 +20,51 @@ class Simulator(object):
     
     def run(self):
         while True:
-            for process in processes:
+            for process in self._processes:
                 proc_id = process.get_id()
                 proc_input = self.get_input(proc_id)
+                if proc_input is None:
+                    continue
+                
                 output = process.process(proc_input)
-                self.sent_output(proc_id, output, processes)
+                self.sent_output(proc_id, output)
 
             next_round = raw_input('next_round[default:1]: ').strip()
             if next_round == 'exit':
                 break
+            elif len(next_round) == 0:
+                next_round = 1
+            else:
+                next_round = int(next_round)
             goto_next_round(next_round)
     
     def get_input(self, proc_id):
-        input = self._input_queues[proc_id][0]
-        self._input_queues[proc_id] = self._input_queues[proc_id][1:]
+        if len(self._input_queues[proc_id]) > 0:
+            return self._input_queues[proc_id][0]
+        else:
+            return None
 
     def sent_output(self, process_id, output):
         for target_process, msg in output.iteritems():
             distance = self._links[(process_id, target_process)]
-            self._input_queues[target_process][distance].append(msg)
+            queue_size = len(self._input_queues[target_process])
+            if queue_size < distance:
+                for _ in range(queue_size, distance):
+                    self._input_queues[target_process].append([])
+            self._input_queues[target_process][distance - 1].append(msg)
 
     def goto_next_round(next_round):
-        pass
+        for proc in self._processes:
+            proc_id = proc.get_id()
+            self._input_queues[proc_id] = self._input_queues[proc_id][next_round:]
 
 def main():
-    processes = []
+    if len(sys.argv) < 2:
+        print 'Usage:', argv[0], 'proc_module'
+        sys.exit(1)
     
-    sim = Simulator(processes, [])
+    proc_mod = importlib.import_module(sys.argv[1])
+    sim = Simulator(proc_mod.processes, proc_mod.links)
     sim.run()
 
 if __name__ == '__main__':
