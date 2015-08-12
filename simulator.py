dcd3638be75d0a1b8a4541900225cc628d36c4c9
Yaolong Huang <airekans@gmail.com>
2015-08-12 15:58:41 +0800

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -15,8 +15,14 @@ class Process(object):
 class Simulator(object):
     def __init__(self, processes, links):
         self._processes = processes
-        self._links = links
-        self._input_queues = [[] for p in processes]
+        self._links = {}
+        for k, v in links.iteritems():
+            id1, id2 = k
+            self._links[(id1, id2)] = v
+            self._links[(id2, id1)] = v
+        
+        self._input_queues = [[{}] for p in processes]  # Initial input
+        self._cur_time = 0
     
     def run(self):
         while True:
@@ -26,17 +32,21 @@ class Simulator(object):
                 if proc_input is None:
                     continue
                 
-                output = process.process(proc_input)
-                self.sent_output(proc_id, output)
+                output = process.process(proc_input, self._cur_time)
+                if output:
+                    self.sent_output(proc_id, output)
 
-            next_round = raw_input('next_round[default:1]: ').strip()
-            if next_round == 'exit':
+            try:
+                next_round = raw_input('next_round[default:1]: ').strip()
+                if next_round == 'exit':
+                    break
+                elif len(next_round) == 0:
+                    next_round = 1
+                else:
+                    next_round = int(next_round)
+                self.goto_next_round(next_round)
+            except EOFError:
                 break
-            elif len(next_round) == 0:
-                next_round = 1
-            else:
-                next_round = int(next_round)
-            goto_next_round(next_round)
     
     def get_input(self, proc_id):
         if len(self._input_queues[proc_id]) > 0:
@@ -45,22 +55,34 @@ class Simulator(object):
             return None
 
     def sent_output(self, process_id, output):
+        distance = 0
         for target_process, msg in output.iteritems():
-            distance = self._links[(process_id, target_process)]
+            try:
+                distance = self._links[(process_id, target_process)]
+            except KeyError:
+                print 'SIM_ERROR: no link from %d to %d, drop msg' % (process_id, target_process)
+                continue
+            
             queue_size = len(self._input_queues[target_process])
-            if queue_size < distance:
-                for _ in range(queue_size, distance):
-                    self._input_queues[target_process].append([])
-            self._input_queues[target_process][distance - 1].append(msg)
+            if queue_size <= distance:
+                for _ in range(queue_size, distance + 1):
+                    self._input_queues[target_process].append({})
+            
+            queue = self._input_queues[target_process][distance]
+            if process_id in queue:
+                queue[process_id].append(msg)
+            else:
+                queue[process_id] = [msg]
 
-    def goto_next_round(next_round):
+    def goto_next_round(self, next_round):
+        self._cur_time += next_round
         for proc in self._processes:
             proc_id = proc.get_id()
             self._input_queues[proc_id] = self._input_queues[proc_id][next_round:]
 
 def main():
     if len(sys.argv) < 2:
-        print 'Usage:', argv[0], 'proc_module'
+        print 'Usage:', sys.argv[0], 'proc_module'
         sys.exit(1)
     
     proc_mod = importlib.import_module(sys.argv[1])
@@ -69,4 +91,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
